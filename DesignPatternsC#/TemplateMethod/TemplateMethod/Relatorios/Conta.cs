f125b1ce850ff55258668bfbe94f28a490228bd0
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-12-21 12:51:02 -0300

    Adicionando exemplo template model

---
+++ DesignPatternsC#/TemplateMethod/TemplateMethod/Relatorios/Conta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Relatorios
+{
+    public class Conta
+    {
+        public int Nome { get; set; }
+        public int Saldo { get; set; }
+        public int Numero { get; set; }
+        public int Agencia { get; set; }
+    }
+}
---
+++ DesignPatternsC#/TemplateMethod/TemplateMethod/Relatorios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Relatorios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
