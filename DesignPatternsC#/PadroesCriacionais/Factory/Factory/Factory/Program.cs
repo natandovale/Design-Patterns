ae407fe196038c6ead84943c1c5f113d97637825
natanaeloliveira <natanaeloliveira@mobills.com.br>
2022-02-22 13:49:43 -0300

    Atualizando pastas

---
+++ DesignPatternsC#/PadroesCriacionais/Factory/Factory/Factory/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "select * from tabela";
+        }
+    }
+}
